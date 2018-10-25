25745a6a9f1b42bb599afe2ca130f31fbab01c15
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-10-24 22:17:59 -0600

    draw a red box in the center of the window

--- myrender.cpp
+++ myrender.cpp
@@ -1,12 +1,36 @@
 #include <glm/glm.hpp>
 #include <stdio.h>
-
 #include "GL/glew.h"
 #include "GL/freeglut.h"
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+void display(void)  {
+    const GLfloat RED[] = {1.0, 0.0, 0.0};
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    glBegin(GL_POLYGON);
+    glColor3fv(RED);
+    glVertex3f(-0.5,0.5,0);
+    glVertex3f(-0.5,-0.5,0);
+    glVertex3f(0.5,-0.5,0);
+    glVertex3f(0.5,0.5,0);
+    glEnd();
+
+    glFlush();
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[]) {
     printf("hello, world!\n");
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("window 1");
+    glutDisplayFunc(display);
+    glutMainLoop();
+
     return 0;
 }
